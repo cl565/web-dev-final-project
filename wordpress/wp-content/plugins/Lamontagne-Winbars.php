e9f708d78b2250b90748454dcd6ffecb8e380f4f
Charlotte L <cl565@georgetown.edu>
2013-07-09 19:12:05 -0400

    added Neighborhood Taxonomy to CPT for wIne bars

--- wordpress/wp-content/plugins/Lamontagne-Winbars.php
+++ wordpress/wp-content/plugins/Lamontagne-Winbars.php
@@ -31,8 +31,35 @@ function cl_create_post_type() {
 		'rewrite'		=> array( 'slug' => 'books' ),
 		'supports' 		=> array( 'title', 'thumbnail', 'editor' )
 	);
-	register_post_type( 'rl_wineBars', $args );
-
+	register_post_type( 'cl_wineBars', $args );
 }
 
+/* Hook in to the init action and call cl_create_book_taxonomies when it fires. */
+add_action( 'init', 'cl_create_book_taxonomies', 0 );
+
+function cl_create_book_taxonomies() {
+	// Add new taxonomy, keep it non-hierarchical (like tags)
+	$labels = array(
+		'name' 							=> __( 'Neighborhoods', 'wineBars' ),
+		'singular_name' 				=> __( 'Neighborhood', 'wineBars' ),
+		'search_items' 					=> __( 'Search Neighborhoods', 'wineBars' ),
+		'all_items' 					=> __( 'All Neighborhoods', 'wineBars' ),
+		'edit_item' 					=> __( 'Edit Neighborhood', 'wineBars' ), 
+		'update_item' 					=> __( 'Update Neighborhood', 'wineBars' ),
+		'add_new_item' 					=> __( 'Add New Neighborhood', 'wineBars' ),
+		'new_item_name' 				=> __( 'New Neighborhood Name', 'wineBars' ),
+		'separate_items_with_commas' 	=> __( 'Separate neighborhoods with commas', 'wineBars' ),
+		'choose_from_most_used' 		=> __( 'Choose from the most used neighborhoods', 'wineBars' ),
+		'menu_name' 					=> __( 'Neighborhoods', 'wineBars' ),
+	); 	
+		
+	register_taxonomy( 'wineBar-neighborhood', array( 'cl_wineBars' ), array(
+		'hierarchical' 		=> false,
+		'labels' 			=> $labels,
+		'show_ui' 			=> true,
+		'show_admin_column' => true,
+		'query_var' 		=> true,
+		'rewrite' 			=> array( 'slug' => 'wineBar-neighborhood' ),
+	));
+}
 ?>

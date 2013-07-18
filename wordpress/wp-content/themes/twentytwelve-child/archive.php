e546170588e6ecd756727bf3c0064f2f6c1a2e3f
Charlotte L <cl565@georgetown.edu>
2013-07-18 11:11:39 -0400

    Removed new code to fix glitch
    Site was not working

--- wordpress/wp-content/themes/twentytwelve-child/archive.php
+++ wordpress/wp-content/themes/twentytwelve-child/archive.php
@@ -56,18 +56,6 @@ get_header(); ?>
 			<?php get_template_part( 'content', 'none' ); ?>
 		<?php endif; ?>
 
-		<?php 
-		add_filter('pre_get_posts', 'query_post_type');
-		function query_post_type($query) {
-  			if(is_category() || is_tag()) {
-    		$post_type = get_query_var('post_type');
-			if($post_type)
-	   		$post_type = $post_type;
-			else
-	    	$post_type = array('cl_winebars', 'post');
-    		$query->set('post_type',$post_type);
-			return $query;
-			?>
     }
 }
 

db6d6dd2ca419c35524128ea152c845c2748d4e7
Charlotte L <cl565@georgetown.edu>
2013-07-20 10:01:52 -0400

    Adding regular posts to the main page

--- wordpress/wp-content/plugins/Lamontagne-Winbars.php
+++ wordpress/wp-content/plugins/Lamontagne-Winbars.php
@@ -69,7 +69,7 @@ add_filter( 'pre_get_posts', 'my_get_posts' );
 
 function my_get_posts( $query ) {
 	if ( is_home() && false == $query->query_vars['suppress_filters'] )
-		$query->set( 'post_type', array( 'cl_winebars' ) );
+		$query->set( 'post_type', array( 'cl_winebars', 'post' ) );
 		return $query;
 }
 
